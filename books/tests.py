28763b8c78fc6f20a9921be2184419c1bbcd3b47
tboelsen <boelsen.tim@gmail.com>
2020-09-09 08:43:27 +0100

    added book user review feature

--- books/tests.py
+++ books/tests.py
@@ -2,17 +2,29 @@ from django.test import TestCase, Client
 
 from django.urls import reverse
 
-from .models import Book
+from .models import Book, Review
+
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
 
 class BookTests(TestCase):
 
     def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username = 'reviewuser',
+            email = 'reviewuser@email.com',
+            password = 'testpass123')
+
         self.book = Book.objects.create(
             title = 'Harry Potter',
             author = 'JK Rowling',
             price = '25.00')
+
+        self.review = Review.objects.create(
+            book = self.book,
+            author = self.user,
+            review = 'Great Book!')
 
     def test_book_listing(self):
         self.assertEqual(f'{self.book.title}', 'Harry Potter')
@@ -31,4 +43,5 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Harry Potter')
+        self.assertContains(response, 'Great Book!')
         self.assertTemplateUsed(response, 'books/book_detail.html')
